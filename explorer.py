b23530dd97aa4498da8d014823511004f2be94ec
Marat Radchenko <marat@slonopotamus.org>
2023-02-25 22:26:05 +0300

    Move helper functions to separate class

--- explorer.py
+++ explorer.py
@@ -23,40 +23,39 @@ class LedColor:
     White = 10
 
 
-class Robot:
-    def __init__(self, vm):
-        self.vm = vm
+class VirtualMachineExt(runtime.VirtualMachine):
+    def __init__(self, rpc, system, stop):
+        super().__init__(rpc, system, stop, None)
 
-    def move_start_forward(self):
-        hub.led(LedColor.Green)
-        movement.move_start(
-            self.vm, movement.from_direction("forward", self.vm.store.move_speed())
+    async def move_cm_async(self, direction: str, cm: float):
+        await movement.move_degrees(
+            self,
+            (cm / self.store.move_calibration()) * 360,
+            movement.from_direction(direction, self.store.move_speed()),
         )
 
-    async def move_cm_async(self, direction, cm):
+    async def move_degrees_async(self, direction: str, degrees: float):
         await movement.move_degrees(
-            self.vm,
-            (cm / self.vm.store.move_calibration()) * 360,
-            movement.from_direction(direction, self.vm.store.move_speed()),
-        )
-
-    async def move_degrees_async(self, direction, degrees):
-        await movement.move_degrees(
-            self.vm,
+            self,
             degrees,
-            movement.from_direction(direction, self.vm.store.move_speed()),
+            movement.from_direction(direction, self.store.move_speed()),
         )
+
+    def start_move_direction(self, direction: str):
+        movement.move_start(self, movement.from_direction(direction, self.store.move_speed()))
 
     def stop_movement(self):
-        movement.move_stop(self.vm)
+        movement.move_stop(self)
 
-    async def run_motor_to_position(self, motor_port, direction, position):
-        speed = abs(self.vm.store.motor_speed(motor_port))
-        (acceleration, deceleration) = self.vm.store.motor_acceleration(motor_port)
-        stall = self.vm.store.motor_stall(motor_port)
-        stop = self.vm.store.motor_stop(motor_port)
-        motor = self.vm.system.motors.on_port(motor_port)
-        self.vm.store.motor_last_status(
+    async def run_motor_to_position(
+        self, motor_port: str, direction: str, position: float
+    ):
+        speed = abs(self.store.motor_speed(motor_port))
+        (acceleration, deceleration) = self.store.motor_acceleration(motor_port)
+        stall = self.store.motor_stall(motor_port)
+        stop = self.store.motor_stop(motor_port)
+        motor = self.system.motors.on_port(motor_port)
+        self.store.motor_last_status(
             await motor.run_to_position_async(
                 position,
                 speed,
@@ -68,17 +67,32 @@ class Robot:
             )
         )
 
-    async def rotate_radar_async(self, direction, position):
-        await self.run_motor_to_position("E", direction, position)
-
-    def get_color(self):
-        result = sensors.get_sensor_value("C", 0, LedColor.Nothing, (61,))
+    def get_color(self, sensor_port: str):
+        result = sensors.get_sensor_value(sensor_port, 0, LedColor.Nothing, (61,))
         return LedColor.Nothing if result is None else result
 
-    def get_proximity(self):
+    def get_proximity(self, sensor_port: str):
         max_dist = 200
-        result = sensors.get_sensor_value("D", 0, max_dist, (62,))
+        result = sensors.get_sensor_value(sensor_port, 0, max_dist, (62,))
         return max_dist if result is None else result
+
+
+class Robot:
+    def __init__(self, vm: VirtualMachineExt):
+        self.vm = vm
+
+    def move_start_forward(self):
+        hub.led(LedColor.Green)
+        self.vm.start_move_direction("forward")
+
+    async def rotate_radar_async(self, direction: str, position: float):
+        await self.vm.run_motor_to_position("E", direction, position)
+
+    def get_color(self):
+        return self.vm.get_color("C")
+
+    def get_proximity(self):
+        return self.vm.get_proximity("D")
 
     async def turn_async(self):
         await self.rotate_radar_async("counterclockwise", 270)
@@ -92,10 +106,9 @@ class Robot:
         hub.led(LedColor.Black)
         await self.rotate_radar_async("shortest", 0)
 
-        if left_distance > right_distance:
-            await self.move_degrees_async("counterclockwise", 155)
-        else:
-            await self.move_degrees_async("clockwise", 155)
+        await self.vm.move_degrees_async(
+            "counterclockwise" if left_distance > right_distance else "clockwise", 155
+        )
 
         self.move_start_forward()
 
@@ -109,15 +122,15 @@ class Robot:
         while True:
             if self.get_color() == LedColor.Azure:
                 hub.led(LedColor.Red)
-                await self.move_cm_async("back", 10)
+                await self.vm.move_cm_async("back", 10)
                 hub.led(LedColor.Black)
                 await self.turn_async()
 
             if self.get_proximity() < 15:
-                self.stop_movement()
+                self.vm.stop_movement()
                 hub.led(LedColor.Yellow)
                 if self.get_proximity() < 10:
-                    await self.move_cm_async("back", 10 * self.get_proximity())
+                    await self.vm.move_cm_async("back", 10 * self.get_proximity())
                 await self.turn_async()
 
             yield
@@ -130,6 +143,6 @@ async def main(vm, stack):
 
 
 def setup(rpc, system, stop):
-    vm = runtime.VirtualMachine(rpc, system, stop, None)
+    vm = VirtualMachineExt(rpc, system, stop)
     vm.register_on_start(None, main)
     return vm
